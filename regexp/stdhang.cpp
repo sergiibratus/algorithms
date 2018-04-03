453e664329a3bed82604104863c7487222eadd15
main <main@mains-mbp.dev.local>
2018-04-03 12:18:28 +0300

    adding bad regexp

---
+++ regexp/stdhang.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <regex>
+
+int main() {
+    std::string s("http://www.msn.com/da-dk/img-resizer/tenant/amp/entityid/AAv396x.img");
+    std::regex r("^http(s)?://([a-zA-Z0-9-]+.)+google(.[a-zA-Z0-9-]+)+/complete/search");
+    if (std::regex_match(s, r))
+        std::cout << "match" << std::endl;
+    else
+        std::cout << "no match" << std::endl;
+
+    return 0;
+}
